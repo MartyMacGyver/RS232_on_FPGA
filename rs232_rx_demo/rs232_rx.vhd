53e574b7893f13b2471cd1df590a6c18e5cc22e0
Martin Falatic <martin@falatic.com>
2015-07-05 03:19:31 -0700

    Updated with more information, syntax cleanups

--- rs232_rx_demo/rs232_rx.vhd
+++ rs232_rx_demo/rs232_rx.vhd
@@ -3,58 +3,63 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
  
 entity rs232_rx is
- GENERIC (
-    frequency : natural;
-    baud : natural );
- Port ( clk : in STD_LOGIC;
+    GENERIC (
+        frequency : natural;
+        baud : natural );
+    PORT (
+        clk : in STD_LOGIC;
         rx : in STD_LOGIC;
         data : out STD_LOGIC_VECTOR (7 downto 0);
-        data_strobe : out STD_LOGIC;
-		led_brd: out STD_LOGIC );
+        data_strobe : out STD_LOGIC;  -- For sim only?
+        led_brd: out STD_LOGIC );
 end rs232_rx;
  
 architecture RTL of rs232_rx is
- signal oversampled_bits : std_logic_vector(39 downto 0);
- signal baud_x4 : unsigned(11 downto 0) := (others => '0');
- signal led_state: Std_logic := '0';
+    signal oversampled_bits : std_logic_vector(39 downto 0);
+    signal baud_x4 : unsigned(11 downto 0) := (others => '0');
+    signal led_state: Std_logic := '0';
 begin
  
 process(clk, led_state)
-   begin
-	led_brd <= led_state;
-      if rising_edge(clk) then
-	     if led_state = '1' then
-			led_state <= '0';
-	     else
-		    led_state <= '1';
-		 end if;
-		 led_brd <= led_state;
-		 
-         data_strobe <= '0';
- 
-         if baud_x4 = 0 then
-            -- If a start bit is seen then capture the data and reset the shift registers
-            if oversampled_bits(39 downto 37) = "000" then
-               data_strobe <= '1';
-               data <= oversampled_bits(34) & oversampled_bits(30) 
-                     & oversampled_bits(26) & oversampled_bits(22)
-                     & oversampled_bits(18) & oversampled_bits(14) 
-                     & oversampled_bits(10) & oversampled_bits( 6); 
-               oversampled_bits(39 downto 1) <= (others => '1');
-               oversampled_bits(0) <= rx;
-            else
-               -- Just capture another bit in the shift register.
-               oversampled_bits <= oversampled_bits(38 downto 0) & rx;
+    begin
+        if rising_edge(clk) then
+            data_strobe <= '0';
+
+            if baud_x4 = 0 then
+                if led_state = '1' then
+                    led_state <= '0';
+                else
+                    led_state <= '1';
+                end if;
+                led_brd <= led_state; 
+
+                -- Process a frame if detected
+                if oversampled_bits(39 downto 37) = "000" then
+                    -- If a start bit is seen then capture the data and reset the shift registers
+                    data_strobe <= '1';
+                    data(0) <= oversampled_bits(34);
+                    data(1) <= oversampled_bits(30);
+                    data(2) <= oversampled_bits(26);
+                    data(3) <= oversampled_bits(22);
+                    data(4) <= oversampled_bits(18);
+                    data(5) <= oversampled_bits(14);
+                    data(6) <= oversampled_bits(10);
+                    data(7) <= oversampled_bits( 6);
+                    -- stop <= oversampled_bits( 2);
+                    oversampled_bits(39 downto 0) <= (0 => rx, others => '1');
+                else
+                    -- Capture another bit and concatenate it to the end of the shift register
+                    oversampled_bits <= oversampled_bits(38 downto 0) & rx;
+                end if;
             end if;
-         end if;
- 
-         -- process the interval counter
-         if baud_x4 = frequency / (baud*4) - 1 then
-            baud_x4 <= (others => '0');
-         else
-            baud_x4 <= baud_x4+1;
-         end if;
-      end if;
-   end process;
+
+            -- process the interval counter
+            if baud_x4 = frequency / (baud*4) - 1 then
+                baud_x4 <= (others => '0');
+            else
+                baud_x4 <= baud_x4+1;
+            end if;
+        end if;
+    end process;
 end RTL;
 
--- rs232_rx_demo/rs232_rx_demo.vhd
+++ rs232_rx_demo/rs232_rx_demo.vhd
@@ -2,27 +2,38 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
  
 entity rs232_rx_demo is
-   Port ( CLK : in STD_LOGIC;
-   RX : in STD_LOGIC;
-   LED : out STD_LOGIC_VECTOR (7 downto 0);
-   LED1 : out std_logic );
+    PORT (
+        CLK : in STD_LOGIC;
+        RX : in STD_LOGIC;
+        LED : out STD_LOGIC_VECTOR (7 downto 0);
+        LED1 : out std_logic );
 end rs232_rx_demo;
  
 architecture RTL of rs232_rx_demo is
-   COMPONENT rs232_rx
-      GENERIC ( frequency : natural;
-                baud : natural );
-      PORT ( clk : IN std_logic;
-             rx : IN std_logic;
-             data : OUT std_logic_vector(7 downto 0);
-             data_strobe : OUT std_logic;
-			 led_brd : OUT std_logic );
-   END COMPONENT;
+    COMPONENT rs232_rx
+        GENERIC (
+            frequency : natural;
+            baud : natural );
+        PORT (
+            clk : IN std_logic;
+            rx : IN std_logic;
+            data : OUT std_logic_vector(7 downto 0);
+            data_strobe : OUT std_logic;
+            led_brd : OUT std_logic );
+    END COMPONENT;
 begin
  
-Inst_rs232_rx: rs232_rx 
-   GENERIC MAP (frequency => 32000000, baud => 9600)
-   PORT MAP(clk => CLK, rx => RX, data => LED, data_strobe => open,  led_brd => LED1);
+Inst_rs232_rx: rs232_rx
+    -- baud = 9600 orig; 115200 is about the max with this clock
+    GENERIC MAP (
+        frequency => 32000000,
+        baud => 115200 )
+    PORT MAP(
+        clk => CLK,
+        rx => RX,
+        data => LED,
+        data_strobe => open,
+        led_brd => LED1 );
  
 end RTL;
 
